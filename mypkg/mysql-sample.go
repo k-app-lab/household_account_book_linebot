6bfc5b66c35975241c2a35b07d1be4cdbdf57a11
kota.tonami <gintama.saicoo@gmail.com>
2022-05-22 12:36:01 +0900

    簡単なものに修正

--- mypkg/mysql-sample.go
+++ mypkg/mysql-sample.go
@@ -24,21 +24,10 @@ func SQLSample() {
 	// 取得件数を条件[$1]にする
 	// ?だとエラーが発生
 	//cmd := "select id, order_id from final_sales where id like $1"
-	cmd := "select id, name from mybook where id like $1"
+	cmd := "select * from mybook"
 	//取得するデータが1件の場合は、QueryRowも利用できる
 	rows, _ := db.Query(cmd, "T00%")
+	fmt.Println(rows)
 	defer rows.Close()
 
-	var sales []Sale // 取得するデータの構造体を用意（複数取得するのでスライス）
-	for rows.Next() {
-		var tmp Sale // 取得の格納用
-		err := rows.Scan(&tmp.id, &tmp.name)
-		if err != nil {
-			log.Fatalln("取得失敗", err)
-		}
-		sales = append(sales, tmp)
-	}
-	for _, sale := range sales {
-		fmt.Println(sale.id, sale.name)
-	}
 }

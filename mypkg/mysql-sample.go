0bf266f2865d76d97dedc1d83af46dd556831268
kota.tonami <gintama.saicoo@gmail.com>
2022-05-26 22:54:55 +0900

    クエリ分を修正

--- mypkg/mysql-sample.go
+++ mypkg/mysql-sample.go
@@ -24,7 +24,7 @@ func SQLSample() {
 	// 取得件数を条件[$1]にする
 	// ?だとエラーが発生
 	//cmd := "select id, order_id from final_sales where id like $1"
-	cmd := "select * from mybook"
+	cmd := "select * from mybook;"
 	//取得するデータが1件の場合は、QueryRowも利用できる
 	rows, _ := db.Query(cmd, "T00%")
 	fmt.Println(rows)

5f74df24c69a7f49707145be3c8e4495f0a8d630
kota.tonami <gintama.saicoo@gmail.com>
2022-05-29 11:23:30 +0900

    不明なメッセージが来たときにランダムで返信するようにした

--- main.go
+++ main.go
@@ -4,11 +4,20 @@ import (
 	"log"
 	"os"
 
+	"math/rand"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/ko-app-lab/household_account_book_linebot/mypkg"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
+
+var householdActions = []linebot.TemplateAction{
+	linebot.NewMessageAction("Say message", "洗濯"),
+	linebot.NewMessageAction("Say message", "掃除"),
+	linebot.NewMessageAction("Say message", "犬の散歩"),
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -46,23 +55,40 @@ func main() {
 					replyMessage := message.Text
 					loginMessage, err := mypkg.FetchLoginMessage(replyMessage)
 					if loginMessage != "" && err == nil {
-						// ログイン時のメッセージ表示
-						// bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(loginMessage)).Do()
 						// ログインできたら家事の選択
-						askTitle := "家事の選択"
-						askDoneHousehold := "終わった家事を選択肢てね！"
-						template := linebot.NewButtonsTemplate("", askDoneHousehold, askTitle, linebot.NewPostbackAction("言 hello2", "hello こんにちは", "hello こんにちは", ""), linebot.NewMessageAction("Say message", "Rice=米"))
+						askTitle := "家事選択"
+						// ログインメッセージと家事選択を促す
+						askDoneHousehold := loginMessage + "\n終わった家事を選択してね！"
+						template := linebot.NewButtonsTemplate("", askDoneHousehold, askTitle, householdActions...)
 						bot.ReplyMessage(event.ReplyToken, linebot.NewTemplateMessage(askTitle, template)).Do()
 					} else {
-						// 上記意外は、おうむ返しで返信
-						_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
-						if err != nil {
-							log.Print(err)
-						}
+						// 上記以外は、不明なメッセージとして返信
+						replyUndefineMessage(bot, event)
 					}
 				}
 			}
 		}
 	})
 	router.Run(":" + port)
+}
+
+// 不明なメッセージを取得したときの返信
+var undefinedMessage = []string{
+	"ちょっと何言っているかわかんない...",
+	"日本語話してもろてもいいですか？",
+	"すみません、よくわかりません",
+	"管理者に使い方教えてもらえ",
+}
+
+func replyUndefineMessage(bot *linebot.Client, event *linebot.Event) {
+	_, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(generateRandamUndefineMessage())).Do()
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func generateRandamUndefineMessage() string {
+	rand.Seed(time.Now().UnixNano())
+	randIndex := rand.Intn((len(undefinedMessage) - 1))
+	return undefinedMessage[randIndex]
 }

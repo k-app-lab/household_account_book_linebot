fe6062947a120aee430fc1c4b738074bdebfb1ff
kota.tonami <gintama.saicoo@gmail.com>
2022-05-29 13:21:52 +0900

    フィールドになかったのが問題か確認

--- main.go
+++ main.go
@@ -12,6 +12,12 @@ import (
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
+var householdActions = []linebot.TemplateAction{
+	linebot.NewMessageAction("洗濯", ",洗濯"),
+	linebot.NewMessageAction("掃除", ",掃除"),
+	linebot.NewMessageAction("犬の散歩", ",犬の散歩"),
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -53,11 +59,6 @@ func main() {
 						// ログインメッセージと家事選択を促す
 						askDoneHousehold := loginMessage + "\n終わった家事を選択してね！"
 						// 「ユーザ名,家事名」の形で送信させる
-						var householdActions = []linebot.TemplateAction{
-							linebot.NewMessageAction("洗濯", ",洗濯"),
-							linebot.NewMessageAction("掃除", ",掃除"),
-							linebot.NewMessageAction("犬の散歩", ",犬の散歩"),
-						}
 						template := linebot.NewButtonsTemplate("", askDoneHousehold, askTitle, householdActions...)
 						bot.ReplyMessage(event.ReplyToken, linebot.NewTemplateMessage(askTitle, template)).Do()
 					} else {

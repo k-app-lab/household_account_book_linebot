f71f95f734df1ec78c4579dcfecd3e24eb390387
kota.tonami <gintama.saicoo@gmail.com>
2022-05-29 13:43:28 +0900

    sqlで取得するデータの形を変更

--- mypkg/mysql-sample.go
+++ mypkg/mysql-sample.go
@@ -14,36 +14,7 @@ type User struct {
 	Id      int
 	Name    string
 	Message string
-}
-
-func SQLSample() {
-	// DBと接続
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatalln("接続失敗", err)
-	}
-	// defer文はreturn前に呼ばれる（スタック実装）
-	defer db.Close()
-
-	cmd := "select * from mybook;"
-	rows, err := db.Query(cmd)
-	if err != nil {
-		log.Fatalln("クエリ取得失敗", err)
-	}
-	defer rows.Close()
-
-	var user User
-	for rows.Next() {
-		err := rows.Scan(&user.Id, &user.Name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("ID: %d, Name: %s\n", user.Id, user.Name)
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+	Point   int
 }
 
 // 入力した文字列でクエリを検索する
@@ -70,7 +41,7 @@ func FetchLoginMessage(key string) (string, error) {
 
 	var user User
 	for rows.Next() {
-		err := rows.Scan(&user.Id, &user.Name, &user.Message)
+		err := rows.Scan(&user.Id, &user.Name, &user.Message, &user.Point)
 		if err != nil {
 			log.Fatal(err)
 			return "", err
